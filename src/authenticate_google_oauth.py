8e1e636f4b36eb00869d75e9619e7d0b32bad220
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2023-04-12 10:21:39 +0900

    Update authenticate_google_oauth.py

--- src/authenticate_google_oauth.py
+++ src/authenticate_google_oauth.py
@@ -5,19 +5,22 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-def auth_google():
-    credential = None
-    if os.path.exists('token.json'):
-        credential = Credentials.from_authorized_user_file('token.json', ['SCOPE'])
+def oauth_google(service):
+    if service == 'image_search':
+        return build('customsearch', 'v1', developerKey='APIトークン')
+    elif service == 'gdrive':
+        credential = None
+        if os.path.exists('token.json'):
+            credential = Credentials.from_authorized_user_file('token.json', ['SCOPE'])
 
-    if not credential or not credential.valid:
-        if credential and credential.expired and credential.refresh_token:
-            credential.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', ['SCOPE'])
-            credential = flow.run_local_server(port=0)
+        if not credential or not credential.valid:
+            if credential and credential.expired and credential.refresh_token:
+                credential.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', ['SCOPE'])
+                credential = flow.run_local_server(port=0)
 
-        with open('token.json', 'w') as token:
-            token.write(credential.to_json())
+            with open('token.json', 'w') as token:
+                token.write(credential.to_json())
 
-    return build('drive', 'v3', credentials=credential)
+        return build('drive', 'v3', credentials=credential)
